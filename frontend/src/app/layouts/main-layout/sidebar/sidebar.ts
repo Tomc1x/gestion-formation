1345ff77566648021bef6e37c0a0d1740f38b3df
Tomy DACALOR <dacalor.tmcx@hotmaiL.com>
2026-06-05 04:28:28 -0400

    feat(frontend): restructure sidebar component with enhanced navigation and styling

--- frontend/src/app/layouts/main-layout/sidebar/sidebar.ts
+++ frontend/src/app/layouts/main-layout/sidebar/sidebar.ts
@@ -1,10 +1,37 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import {
+  LucideGraduationCap,
+  LucideLifeBuoy,
+  LucideCalendar,
+  LucideUsers,
+  LucideDynamicIcon,
+  } from '@lucide/angular';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, LucideDynamicIcon,LucideGraduationCap, LucideLifeBuoy, LucideCalendar, LucideUsers],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class SidebarComponent {}
+export class SidebarComponent {
+  protected readonly routes: {path: string, label: string, icon: any}[]= [
+    {
+      path: '/app/dashboard',
+      label: 'Tableau de bord',
+      icon: LucideGraduationCap
+    },
+    {
+      path: '/app/promotions',
+      label: 'Promotions',
+      icon: LucideUsers
+    },
+    {
+      path: '/app/Calendrier',
+      label: 'Calendrier',
+      icon: LucideCalendar
+    },
+  ]
+}
